eaee76653c4eb60731fcd1ef7a010d4114e8c47c
Ítalo Felipe <italo.felipe@exaengenharia.com>
2025-04-15 15:29:52 -0300

    💾 Salvando alterações locais automáticas

--- git-sync.bat
+++ git-sync.bat
@@ -1,8 +1,12 @@
 @echo off
 chcp 65001 >nul
+
 echo =================================
 echo 🔄 Sincronizando com o GitHub...
 echo =================================
+
+git add . >nul
+git commit -m "💾 Salvando alterações locais automáticas" >nul 2>nul
 
 git pull origin main --rebase
 
